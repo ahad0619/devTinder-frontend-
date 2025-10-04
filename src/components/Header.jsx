f361d25372e0cfd94113069e3df5d159501db78d
Your Name <your_email@example.com>
2025-10-04 16:07:33 +0530

    logut added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
-import { useSelector } from "react-redux"
-import { Link } from "react-router-dom"
+import axios from "axios"
+import { useDispatch, useSelector } from "react-redux"
+import { Link, useNavigate } from "react-router-dom"
+import { Base_URL } from "../utils/constants"
+import { removeUser } from "../utils/userSlice"
 
 
 const Header = () => {
-  const user= useSelector((store)=>store.user)
-  
+  const user = useSelector((store) => store.user)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleClick = async () => {
+    try {
+      await axios.post(Base_URL + "logout", {},
+        { withCredentials: true })
+      dispatch(removeUser())
+      return navigate("/login")
+    } catch (err) {
+      console.log(err)
+    }
+
+  }
+
   return (
     <div>
       <div className="navbar bg-base-400 shadow-lg">
@@ -12,15 +29,15 @@ const Header = () => {
           <a className="btn btn-ghost text-xl">devTinder</a>
         </div>
         <div className="flex gap-2">
-        
+
           <div className="dropdown dropdown-end">
             {user && <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar mr-6">
               <div className="w-10 rounded-full ">
                 <img
-                
+
                   alt="Tailwind CSS Navbar component"
                   src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                   />
+                />
               </div>
             </div>}
             <ul
@@ -33,7 +50,9 @@ const Header = () => {
                 </Link>
               </li>
               <li><a>Settings</a></li>
-              <li><a>Logout</a></li>
+              <li>
+                <a onClick={handleClick}>Logout</a>
+              </li>
             </ul>
           </div>
         </div>
